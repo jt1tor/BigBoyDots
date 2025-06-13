54849e83d309ae2138fb9bba0bdd644dd3eb41bb
jt1t0r <jt1t0r@proton.me>
2025-06-12 23:05:37 -0400

    system update

--- flake.nix
+++ flake.nix
@@ -2,23 +2,28 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+#####    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    # home manager
+    home-manager = {
+     url = "github:nix-community/home-manager/master";
+     inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     # hyprland
     hyprland.url = "github:hyprwm/Hyprland";
     hyprland-plugins = {
       url = "github:hyprwm/hyprland-plugins";
       inputs.hyprland.follows = "hyprland";
     };
+
     # nyaa
     nyaa = {
       url = "github:Beastwick18/nyaa";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # home manager
-    home-manager = {
-     url = "github:nix-community/home-manager/release-25.11";
-     inputs.nixpkgs.follows = "nixpkgs";
-    };
+
   };
 
   outputs = { self, nixpkgs, home-manager,  ... }@inputs:
