d4ad3e60f33292371fc16ba4eaa5ead960b4dd72
jt1t0r <jt1t0r@proton.me>
2025-06-15 11:42:16 -0400

    install obs

--- configuration.nix
+++ configuration.nix
@@ -12,7 +12,7 @@
 
   # Make some extra kernel modules available to NixOS
   boot.extraModulePackages = with config.boot.kernelPackages;
-    [ v4l2loopback.out ];
+    [ v4l2loopback ];
 
   # Activate kernel modules (choose from built-ins and extra ones)
   boot.kernelModules = [
@@ -25,9 +25,6 @@
 
   # Set initial kernel module settings
   boot.extraModprobeConfig = ''
-    # exclusive_caps: Skype, Zoom, Teams etc. will only show device when actually streaming
-    # card_label: Name of virtual camera, how it'll show up in Skype, Zoom, Teams
-    # https://github.com/umlaeute/v4l2loopback
     options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
   '';
 
@@ -314,6 +311,7 @@
   avahi
   alsa-utils
   kdePackages.kdenlive
+  obs-studio
   ];
 
   # fonts
