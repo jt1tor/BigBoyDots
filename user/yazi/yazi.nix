d38289b96df6977a62824f909153e6f55ad1417c
jt1tor <jt1t0r@proton.me>
2025-07-05 01:04:43 -0400

    edit yazi module

--- user/yazi/yazi.nix
+++ user/yazi/yazi.nix
@@ -4,9 +4,39 @@
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
-    plugins = with pkgs.yaziPlugins; [
-      mount
-      full-border
-    ];
+    plugins = {
+      mount = pkgs.yaziPlugins.mount;
+      full-border = pkgs.yaziPlugins.full-border;
+    };
+
+    keymap = {
+      # Mount Plugin
+      mgr.prepend_keymap = [
+        { run = "plugin mount"; on = "M"; }
+      ];
+    };
+
+    settings = {
+      mgr = {
+        show_hidden = true;
+        sort_by = "alphabetical";
+        sort_dir_first = true;
+        show_symlink = true;
+        scrolloff = 5;
+        linemode = "mtime";
+      };
+      opener = {
+        edit = [
+          { run = "nvim \"$@\""; desc = "$EDITOR"; block = true; for = "unix"; }
+        ];
+      };
+    };
+
+    initLua = ''
+      require("full-border"):setup {
+        -- Available: PLAIN or ROUNDED
+        type = ui.Border.PLAIN,
+      }
+    '';
   };
 }
